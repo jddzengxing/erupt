4a86ae62b9c71409bc5c5fa42927ec036c03de8d
jddzengxing <jddzengxing@163.com>
2025-05-29 11:06:26 +0800

    commit

---
+++ erupt-annotation/src/main/java/xyz/erupt/annotation/fun/AttachmentProxy.java
@@ -0,0 +1,25 @@
+package xyz.erupt.annotation.fun;
+
+import xyz.erupt.annotation.config.Comment;
+
+import java.io.InputStream;
+
+/**
+ * @author YuePeng
+ * date 2020-05-18
+ */
+@Comment("自定义附件上传策略")
+public interface AttachmentProxy {
+
+    @Comment("附件上传")
+    @Comment("返回值表示存储路径，多数情况下返回 path 即可")
+    String upLoad(@Comment("数据流") InputStream inputStream, @Comment("上传位置") String path);
+
+    @Comment("附件所在域名")
+    String fileDomain();
+
+    @Comment("是否同时保存到本地服务器")
+    default boolean isLocalSave() {
+        return true;
+    }
+}
