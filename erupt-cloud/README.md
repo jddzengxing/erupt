6ec188347570cac5c053af5734c79091c74b90b6
jddzengxing <jddzengxing@163.com>
2025-08-15 09:42:14 +0800

    add files

---
+++ erupt-cloud/README.md
@@ -0,0 +1,4 @@
+﻿# erupt-cloud
+erupt remote develop support
+
+使用文档：[https://www.yuque.com/erupts/erupt/ggbcv9](https://www.yuque.com/erupts/erupt/ggbcv9)
---
+++ erupt-upms/src/main/java/xyz/erupt/upms/model/EruptPost.java
@@ -0,0 +1,49 @@
+package xyz.erupt.upms.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import xyz.erupt.annotation.Erupt;
+import xyz.erupt.annotation.EruptField;
+import xyz.erupt.annotation.EruptI18n;
+import xyz.erupt.annotation.constant.AnnotationConst;
+import xyz.erupt.annotation.sub_field.Edit;
+import xyz.erupt.annotation.sub_field.View;
+import xyz.erupt.annotation.sub_field.sub_edit.Search;
+import xyz.erupt.jpa.model.BaseModel;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ * @author YuePeng
+ * date 2018-11-22.
+ */
+@Entity
+@Table(name = "e_upms_post")
+@Erupt(name = "岗位管理", orderBy = "weight desc")
+@EruptI18n
+@Getter
+@Setter
+public class EruptPost extends BaseModel {
+
+    @Column(length = AnnotationConst.CODE_LENGTH, unique = true)
+    @EruptField(
+            views = @View(title = "岗位编码", sortable = true),
+            edit = @Edit(title = "岗位编码", notNull = true, search = @Search(vague = true))
+    )
+    private String code;
+
+    @EruptField(
+            views = @View(title = "岗位名称", sortable = true),
+            edit = @Edit(title = "岗位名称", notNull = true, search = @Search(vague = true))
+    )
+    private String name;
+
+    @EruptField(
+            views = @View(title = "岗位权重"),
+            edit = @Edit(title = "岗位权重", desc = "数值越高，岗位级别越高")
+    )
+    private Integer weight;
+
+}
